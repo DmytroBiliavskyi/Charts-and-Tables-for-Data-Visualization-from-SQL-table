ce0046152c470a4f183636db75fbec9cbe5b112c
NinjaCoder0516 <haotian.song.0516@gmail.com>
2024-02-12 22:44:13 +0800

    .env

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,15 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "fedev",
-  host: "54.92.200.80",
-  database: "productdb_backup",
-  password: "password",
-  port: 5432,
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT,
   idleTimeoutMillis: 30000,
   max: 20,
 });
